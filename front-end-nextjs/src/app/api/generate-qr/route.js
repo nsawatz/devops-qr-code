9119ad8aa95883f318c4d8eb693714dbd8087e43
nsawatz <nsawatz@wgu.edu>
2025-01-31 21:19:41 -0700

    eks updates

---
+++ front-end-nextjs/src/app/api/generate-qr/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export async function POST(request) {
+  const { searchParams } = new URL(request.url);
+  const url = searchParams.get('url');
+  
+  if (!url) {
+    return NextResponse.json({ error: 'URL is required' }, { status: 400 });
+  }
+
+
+  try {
+    const response = await axios.post(`http://qr-api-service/generate-qr`, null, {
+      params: { url: encodeURIComponent(url) }
+    });
+    console.log('QR Code API Response:', response.data);
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error('Error generating QR Code:', error.message);
+    return NextResponse.json({ error: error.message || 'Failed to generate QR Code' }, { status: 500 });
+  }
+}
